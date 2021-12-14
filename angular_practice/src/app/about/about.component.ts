ef3e87afb8c88b97d0883c760807c46a8928eee2
ianeiros <naxo85@gmail.com>
2021-12-14 18:57:06 +0100

    assigment 2.2

--- angular_practice/src/app/about/about.component.ts
+++ angular_practice/src/app/about/about.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LeaderService } from '../services/leader.service';
+import { Leader } from '../shared/leader';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  constructor(private leaderService: LeaderService) {}
 
-  constructor() { }
+  leaders!: Leader[];
 
   ngOnInit(): void {
+    this.leaders = this.leaderService.getLeaders();
   }
-
 }
--- angular_practice/src/app/dishdetail/dishdetail.component.ts
+++ angular_practice/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -7,9 +11,16 @@ import { Dish } from '../shared/dish';
   styles: [],
 })
 export class DishdetailComponent implements OnInit {
-  constructor() {}
+  constructor(private dishservice: DishService, private route: ActivatedRoute, private location: Location) {}
 
-  ngOnInit(): void {}
+  dish!: Dish;
 
-  @Input() dish!: Dish | undefined;
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
